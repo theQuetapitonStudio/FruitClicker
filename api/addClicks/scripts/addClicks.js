55f36f048fd562cc68f34effda9bbfb31e35547b
quetapiton <oamost123@gmail.com>
2025-10-04 17:17:12 -0300

    Update addClicks.js

--- api/addClicks/scripts/addClicks.js
+++ api/addClicks/scripts/addClicks.js
@@ -5,16 +5,16 @@ let clicks = data.clicks
 
 function addClicks(a) {
     clicks += a
+    data.clicks = clicks
+    localStorage.setItem("fruitClickerData", JSON.stringify(data))
 }
 
 addClicks(100)
 
-function irparaFC( ) {
-    window.location.href="https://thequetapitonstudio.github.io/FruitClicker/index.html"
+function irparaFC() {
+    window.location.href = "https://thequetapitonstudio.github.io/FruitClicker/index.html"
 }
-
 
 setTimeout(() => {
     irparaFC()
 }, 3000)
-
